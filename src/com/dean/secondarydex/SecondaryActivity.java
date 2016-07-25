84e6653e63848c176667d0cc492dab93434214a3
Dean <kiki198858@gmail.com>
2016-07-25 17:22:07 +0800

    create project

---
+++ src/com/dean/secondarydex/SecondaryActivity.java
@@ -0,0 +1,17 @@
+package com.dean.secondarydex;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by chw on 2016/7/22.
+ */
+public class SecondaryActivity extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+
+}
